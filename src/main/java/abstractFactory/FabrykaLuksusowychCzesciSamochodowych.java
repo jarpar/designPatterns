47db8d025ab9e5ec9142ca0690943c09190874b9
JaroslawPartyka <partyka.jaroslaw@gmail.com>
2020-08-29 14:02:24 +0200

    refactored

---
+++ src/main/java/abstractFactory/FabrykaLuksusowychCzesciSamochodowych.java
@@ -0,0 +1,18 @@
+package abstractFactory;
+
+public class FabrykaLuksusowychCzesciSamochodowych implements FabrykaCzesciSamochodowych{
+    @Override
+    public Silnik utworzSilnik() {
+        return null;
+    }
+
+    @Override
+    public Swiatla utwórzSwiatla() {
+        return null;
+    }
+
+    @Override
+    public Opony utworzOpony() {
+        return null;
+    }
+}
---
+++ src/main/java/abstractFactory/Program.java
@@ -0,0 +1,17 @@
+package abstractFactory;
+
+public class Program {
+    public static void main(String[] args) {
+        FabrykaCzesciSamochodowych fabrykaCzesciSamochodowych = new FabrykaEkonomicznychCzesciSamochodowych();
+        Silnik silnik = fabrykaCzesciSamochodowych.utworzSilnik();
+        Swiatla swiatla = fabrykaCzesciSamochodowych.utwórzSwiatla();
+        Opony opony = fabrykaCzesciSamochodowych.utworzOpony();
+
+        System.out.println();
+
+        fabrykaCzesciSamochodowych = new FabrykaLuksusowychCzesciSamochodowych();
+        silnik = fabrykaCzesciSamochodowych.utworzSilnik();
+         swiatla = fabrykaCzesciSamochodowych.utwórzSwiatla();
+         opony = fabrykaCzesciSamochodowych.utworzOpony();
+    }
+}
